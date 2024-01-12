e48719b144ccbf6c781e60a58a39bfdf27b37c6c
YashK-27 <kankriyayash18@gmail.com>
2024-01-12 17:54:56 +0530

    Prediction pipeline and UI/UX done

--- src/utils.py
+++ src/utils.py
@@ -53,4 +53,13 @@ def evaluate_model(X_train,Y_train, X_test, Y_test,models, param):
     
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e,sys)
 
